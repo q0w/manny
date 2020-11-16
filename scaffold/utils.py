bdaca8d7ce489485db91f8da34762e4965b8634e
q0w <green1356@yandex.ru>
2020-11-16 07:00:12 +0300

    hotfix

--- scaffold/utils.py
+++ scaffold/utils.py
@@ -1,5 +1,4 @@
 import functools
-from string import Template
 
 
 def default_kwargs(**defaultKwargs):
@@ -12,34 +11,3 @@ def default_kwargs(**defaultKwargs):
         return g
 
     return actual_decorator
-
-
-class FieldTemplate(Template):
-    template = "$name = models.${type}Field()"
-
-    def __init__(self):
-        super().__init__(self.template)
-
-    def safe_replace(self, **kwargs):
-        try:
-            return self.substitute(**kwargs)
-        except KeyError:
-            raise SystemExit('Provide field name...')
-        except Exception as e:
-            print(e)
-
-
-class DecimalFieldTemplate(FieldTemplate):
-    template = "$name = models.${type}Field(max_digits=${max}, decimal_places=${places})"
-
-    @default_kwargs(max=5, places=2)
-    def safe_replace(self, **kwargs):
-        return super().safe_replace(**kwargs)
-
-
-class CharFieldTemplate(FieldTemplate):
-    template = "$name = models.${type}Field(max_length=${max})"
-
-    @default_kwargs(max=255)
-    def safe_replace(self, **kwargs):
-        return super().safe_replace(**kwargs)
